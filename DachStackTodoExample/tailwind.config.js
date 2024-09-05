ee0a242e73066724e684e262caf454e2954f0a7d
kristen <kristenisadude@gmail.com>
2024-09-05 16:32:08 -0400

    Leaner repo

---
+++ DachStackTodoExample/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+      './wwwroot/**/*.{html,js}',
+      './public/index.html',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [
+      require('daisyui'),
+    ],
+  }
+  
