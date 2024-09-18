e7c520a301693cdfc9adf744f4ff12ab8e534cf0
kristen <kristenisadude@gmail.com>
2024-09-18 00:16:24 -0400

    fixed bug with multi part/chunked upload

--- DachStackTodoExample/api/FileController.cs
+++ DachStackTodoExample/api/FileController.cs
@@ -19,6 +19,8 @@ namespace DachStackApp.api
         {
             _configuration = configuration;
             _containerName = containerName;
+            _configuration["AzureAccountName"] = "devstoreaccount1";
+            _configuration["AzureAccountKey"] = "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==";
             Console.WriteLine($@"{_configuration.ToString()}");
             string storageConnectionString = @"DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;";
             _blobServiceClient = new BlobServiceClient(storageConnectionString);
