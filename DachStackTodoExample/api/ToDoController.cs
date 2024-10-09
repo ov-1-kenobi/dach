1c9fad9cf888b2ab2f1b06ddf4b79dff3191955f
kristen <kristenisadude@gmail.com>
2024-10-09 18:19:41 -0400

    WIP: can add and list table objects, working on deleting

--- DachStackTodoExample/api/ToDoController.cs
+++ DachStackTodoExample/api/ToDoController.cs
@@ -8,9 +8,23 @@ using Azure;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json;
 using System.Runtime.InteropServices;
+using System.Text.Json.Serialization;
+using System.Runtime.CompilerServices;
 
 namespace DachStackApp.api
 {
+    public static class StorageTableExtensions
+    {
+        public static ToDoItem getObjectValue(this StorageEntity storageEntity)
+        {
+            return storageEntity.JsonValue == null ? null : JsonSerializer.Deserialize<ToDoItem>(storageEntity.JsonValue);
+        }
+        public static void setObjectValue(this StorageEntity storageEntity, ToDoItem value)
+        {
+            storageEntity.JsonValue = JsonSerializer.Serialize(value);
+        }
+
+    }
     public class StorageEntity : ITableEntity
     {
         public string PartitionKey { get; set; }
@@ -19,12 +33,7 @@ namespace DachStackApp.api
         public DateTimeOffset? Timestamp { get; set; }
         public ETag ETag { get; set; }
         public string JsonValue { get; set; }
-        [NotMapped]
-        public ToDoItem ObjectValue
-        {
-            get => JsonValue == null ? null : JsonSerializer.Deserialize<ToDoItem>(JsonValue);
-            set => JsonValue = JsonSerializer.Serialize(value);
-        }
+        
     }
     public class ToDoItem
     {
@@ -66,7 +75,7 @@ namespace DachStackApp.api
 
             foreach (StorageEntity item in items)
             {
-                retHTML += $"<li id='todo-{item.ObjectValue.Id}' class='flex items-center justify-between bg-white p-3 rounded shadow'><span>{item.ObjectValue.Task}</span><div><button hx-delete='/api/todo/{item.ObjectValue.Id}' hx-target='closest li' hx-swap='outerHTML' class='btn btn-error btn-xs'>Delete</button><button hx-target='closest li' hx-swap='outerHTML' hx-patch='/api/todo/{item.ObjectValue.Id}' hx-vals='{{\"IsComplete\":true}}' class='btn btn-success btn-xs'>Complete</button></div></li>";
+                retHTML += $"<li id='todo-{item.getObjectValue().Id}' class='flex items-center justify-between bg-white p-3 rounded shadow'><span>{item.getObjectValue().Task}</span><div><button hx-delete='/api/todo/{item.getObjectValue().Id}' hx-target='closest li' hx-swap='outerHTML' class='btn btn-error btn-xs'>Delete</button><button hx-target='closest li' hx-swap='outerHTML' hx-patch='/api/todo/{item.getObjectValue().Id}' hx-vals='{{\"IsComplete\":true}}' class='btn btn-success btn-xs'>Complete</button></div></li>";
             }
             return Ok(retHTML);
         }
@@ -80,8 +89,9 @@ namespace DachStackApp.api
             StorageEntity storageEntity = new StorageEntity() {
                 PartitionKey = APARTITIONKEY, //principal tenant info
                 RowKey = item.Id.ToString(), //UID; int for now
-                ObjectValue = item
+                //ObjectValue = item
             };
+            storageEntity.setObjectValue(item); 
             // item.Id = _items.Count + 1;
             // _items.Add(item);
             tableClient.AddEntity<StorageEntity>(storageEntity);
